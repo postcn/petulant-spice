53e774f11abe92e7074e4fca2c7ec07bd3946916
Caleb Post <postcn@rose-hulman.edu>
2015-04-16 23:27:59 -0400

    Camera Follows. Can fall off edge now.

---
+++ Reap v1/Reap/Assets/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public static CameraFollow S;
+
+	public Transform targetTransform;
+	public float camEasing = 0.1f;
+	Vector3 followOffset = new Vector3(0,10,0);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Awake() {
+		S = this;
+	}
+
+	void FixedUpdate() {
+		Vector3 pos = targetTransform.position+followOffset;
+		transform.position = Vector3.Lerp (transform.position, pos, camEasing);
+	}
+}
