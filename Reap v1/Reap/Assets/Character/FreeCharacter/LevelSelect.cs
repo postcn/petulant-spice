f7bde9b96bed9603fde6d60d798495dc50ed5c7d
goldthea <goldthea@rose-hulman.edu>
2015-05-03 21:33:26 -0400

    added title screen

---
+++ Reap v1/Reap/Assets/Character/FreeCharacter/LevelSelect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void GoToGorgoron(){
+        Application.LoadLevel ("Gorgoron7"); 
+    }
+
+    public void GoToCave(){
+        Application.LoadLevel ("CoolCave"); 
+    }
+
+    public void GoToHome(){
+        Application.LoadLevel ("TitleScreen");
+    }
+}
