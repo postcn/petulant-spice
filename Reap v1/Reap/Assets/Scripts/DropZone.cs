6ff46bd103ff760e87fb9ddeb5d4ce9cec6f5f79
Caleb Post <postcn@rose-hulman.edu>
2015-05-07 13:02:48 -0400

    Drop Ship.

---
+++ Reap v1/Reap/Assets/Scripts/DropZone.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropZone : MonoBehaviour {
+    private const int RADIUS = 2;
+    public GameObject drop;
+    private bool activated = false;
+
+	// Use this for initialization
+	void Start () {
+        DropZoneManager.self.register(this);
+        var renderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in renderers) {
+            r.enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (activated && Hero_Management.self != null) {
+            float f = distance(drop.transform.position, Hero_Management.self.transform.position);
+            if (f < RADIUS) {
+                Debug.Log("Drop ship pickup!");
+            }
+        }
+	}
+
+    private float distance(Vector3 point1, Vector3 point2) {
+        float x = Mathf.Pow(point1.x - point2.x, 2);
+        float y = Mathf.Pow(point1.y - point2.y, 2);
+        float z = Mathf.Pow(point1.z - point2.z, 2);
+        return Mathf.Sqrt(x+y+z);
+    }
+
+    public void activate() {
+        activated = true;
+        var renderers = GetComponentsInChildren<Renderer>();;
+        foreach (Renderer r in renderers) {
+            r.enabled = true;
+        }
+    }
+}
---
+++ Reap v1/Reap/Assets/Scripts/DropZoneManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DropZoneManager : MonoBehaviour {
+    public const int SAMPLES_NEEDED = 500;
+    public static DropZoneManager self;
+    IList<DropZone> dropZones;
+    private bool active = false;
+
+    private const int MIN_DROP_WAIT = 60;
+    private const int MAX_DROP_WAIT = 240;
+
+    public AudioClip[] dropShipInbound;
+    public AudioClip[] dropShipDelayed;
+    public AudioClip[] dropShipArrived;
+
+	// Use this for initialization
+	void Start () {
+        self = this;
+        dropZones = new List<DropZone>();
+	}
+
+    void Update() {
+        if (Hero_Management.self != null && Hero_Management.self.getSamplesCollected() > SAMPLES_NEEDED) {
+            if (!active) {
+                sendDropShip();
+            }
+        }
+    }
+	
+
+    public void register(DropZone zone) {
+        dropZones.Add(zone);
+    }
+
+    void sendDropShip() {
+        active = true;
+        System.Random r = new System.Random();
+        DropZone zone = dropZones[r.Next(dropZones.Count)];
+        int wait = r.Next(MIN_DROP_WAIT, MAX_DROP_WAIT);
+        playRandomAudio(dropShipInbound);
+        StartCoroutine(DropShip(zone, wait));
+    }
+
+    IEnumerator DropShip(DropZone zone, int wait) {
+        yield return new WaitForSeconds(wait);
+        System.Random r = new System.Random();
+        if (r.Next(2) == 0) {
+            playRandomAudio(dropShipDelayed);
+            yield return new WaitForSeconds(r.Next(MIN_DROP_WAIT, MAX_DROP_WAIT));
+        }
+        playRandomAudio(dropShipArrived);
+        zone.activate();
+    }
+
+    void playRandomAudio(AudioClip[] clips) {
+        System.Random rnd = new System.Random();
+        int index = rnd.Next(clips.Length);
+        AudioSource.PlayClipAtPoint(clips[index], Camera.main.transform.position);
+    }
+}
